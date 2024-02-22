56125318b437669296cf3832e8b91efd2620fbc6
Dominic Lacaille <dlacaille@solution-tek.com>
2024-02-22 16:05:06 -0500

    Fix diary paths :bug:

--- src/pop-diary-entry.ts
+++ src/pop-diary-entry.ts
@@ -1,19 +1,21 @@
 import fs from "node:fs"
 import readLastLines from "read-last-lines"
 import diaryFilename from "./utils/diary-filename"
+import dirPath from "./utils/dir-path"
 
 export default function popDiaryEntry(roomId: string, sender: string) {
   const filename = diaryFilename(roomId)
+  const path = dirPath(filename)
 
-  readLastLines.read(filename, 1).then((line: string) => {
+  readLastLines.read(path, 1).then((line: string) => {
     if (line.indexOf(sender) !== 0) {
       // Do not pop the entry if the sender is wrong
       return
     }
 
-    fs.stat(filename, (error, stats) => {
+    fs.stat(path, (error, stats) => {
       if (error) throw error
-      fs.truncate(filename, stats.size - line.length, (error) => {
+      fs.truncate(path, stats.size - line.length, (error) => {
         if (error) throw error
         console.log("Truncated last line from " + filename)
       })
--- src/print-diary.ts
+++ src/print-diary.ts
@@ -1,10 +1,13 @@
 import readLastLines from "read-last-lines"
 import client from "./client"
 import diaryFilename from "./utils/diary-filename"
+import dirPath from "./utils/dir-path"
 
 export default function printDiary(roomId: string, lines = 10) {
   const filename = diaryFilename(roomId)
-  readLastLines.read(filename, lines).then((text: string) => {
+  const path = dirPath(filename)
+
+  readLastLines.read(path, lines).then((text: string) => {
     client.sendNotice(roomId, `\`\`\`\n${text}\n\`\`\``)
   })
 }
